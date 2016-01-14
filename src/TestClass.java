117a5a17ee07b659ac881a9386cd3bca3ade9bd9
Rahul <rahul@home>
2016-01-14 13:31:56 +0530

    add method with two parameter

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,10 +1,25 @@
 /**
- * Created by family on 1/14/2016.
+ method overloading.
  */
+class add {
+
+    void  Add(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
+    }
+    void  Add(int a, int b,int c) {
+        int d = a + b + c;
+        System.out.println(d);
+    }
+}
 public class TestClass {
     public static void main(String[] args){
 
-        System.out.println("hello world");
+        Add a=new Add();
+        a.Add(3,4);
+        a.Add(3,4,6);
+
     }
+
 
 }
